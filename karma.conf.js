5e8294b9ab9ee61ff4d7740c61f2c895c3e6465e
Anthony Maton <maitre.negi@gmail.com>
2017-06-21 13:55:44 +0200

    Setup testing environment

--- karma.conf.js
+++ karma.conf.js
@@ -10,26 +10,27 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'browserify'],
 
 
     // list of files / patterns to load in the browser
     files: [
-      'lib/*.js',
-      'tests/*.js'
+      'lib/span-count.js',
+      'lib/doctests.js',
+      'tests/test-span-count.js'
     ],
 
 
     // list of files to exclude
     exclude: [
-      'tests/testutils.js',
-      'tests/testrunner.js'
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'lib/*.js': ['browserify'],
+        'tests/test-span-count.js': ['browserify']
     },
 
 
@@ -58,7 +59,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: [],
+    browsers: ['Chrome', 'Firefox'],
 
 
     // Continuous Integration mode
