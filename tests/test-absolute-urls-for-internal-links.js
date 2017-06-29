bd8dfec00cd7f47e2c4165b65bb7005ada65e4d9
Anthony Maton <maitre.negi@gmail.com>
2017-06-29 10:01:30 -0700

    Test absoluteURLsForInternalLinks linting

--- tests/test-absolute-urls-for-internal-links.js
+++ tests/test-absolute-urls-for-internal-links.js
@@ -33,37 +33,3 @@ describe('absoluteURLsForInternalLinks', function() {
   });
 });
 
-/*const {ERROR, WARNING, url, runTests} = require("./testutils");
-
-exports["test doc absoluteURLsForInternalLinks"] = function testAbsoluteURLsForInternalLinks(assert, done) {
-  const tests = [
-    {
-      str: '<a href="/en-US/docs/some/page">Page</a>' +
-           '<a href="#anchor">Anchor</a>' +
-           '<a name="anchor">Anchor</a>' +
-           '<a href="https://developer.mozilla.org/en-US/docs/some/page">Anchor</a>' +
-           '<a href="http://developer.mozilla.org/en-US/docs/some/page">Anchor</a>' +
-           '<a href="//developer.mozilla.org/en-US/docs/some/page">Anchor</a>',
-      expected: [
-        {
-          msg: '<a href="https://developer.mozilla.org/en-US/docs/some/page">Anchor</a>',
-          type: WARNING
-        },
-        {
-          msg: '<a href="http://developer.mozilla.org/en-US/docs/some/page">Anchor</a>',
-          type: WARNING
-        },
-        {
-          msg: '<a href="//developer.mozilla.org/en-US/docs/some/page">Anchor</a>',
-          type: WARNING
-        }
-      ],
-      expectedAfterFixing: []
-    }
-  ];
-
-  runTests(assert, done, "absoluteURLsForInternalLinks", "absolute URLs for internal links", url, tests);
-};
-
-require("sdk/test").run(exports);
-*/
