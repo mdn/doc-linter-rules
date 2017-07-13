89cad6b7cf8683e1644bacc8d02aa1ee328e616c
Anthony Maton <maitre.negi@gmail.com>
2017-07-13 14:42:23 +0200

    API headlines: "Exceptions thrown" not marked as invalid

--- tests/test-api-syntax-headlines.js
+++ tests/test-api-syntax-headlines.js
@@ -3,15 +3,16 @@ const apiSyntaxHeadlines = require('../lib/api-syntax-headlines.js').apiSyntaxHe
 const ERROR = require('../lib/doctests.js').ERROR;
 
 describe('apiSyntaxHeadlines', function() {
-  it('Should return 4 apiSyntaxHeadlines errors', function(done) {
+  it('Should return 5 apiSyntaxHeadlines errors', function(done) {
     const str = '<h2 id="Syntax">Syntax</h2>' +
-      '<h3>Errors</h3>' +
+      '<h3>Exceptions thrown</h3>' +
       '<h3>Returns</h3>' +
-      '<h3>Parameters</h3>';
+      '<h3>Arguments</h3>';
 
     const expected = [
-      {msg: "invalid_headline_name", msgParams: ["Errors"], type: ERROR},
+      {msg: "invalid_headline_name", msgParams: ["Exception thrown"], type: ERROR},
       {msg: "invalid_headline_name", msgParams: ["Returns"], type: ERROR},
+      {msg: "invalid_headline_name", msgParams: ["Arguments"], type: ERROR},
       {msg: "invalid_headline_order", type: ERROR},
       {msg: "invalid_headline_order", type: ERROR}
     ];
@@ -21,7 +22,31 @@ describe('apiSyntaxHeadlines', function() {
       {msg: "invalid_headline_order", type: ERROR}
     ];
 
-    
+
+    let rootElement = document.createElement("body");
+    rootElement.innerHTML = str;
+
+    let results = apiSyntaxHeadlines.check(rootElement);
+
+    results.forEach((element, index) => {
+      if(element.msg != expected[index].msg || element.type != expected[index].type)
+        done(Error('Expected : ' + JSON.stringify(expected[index]) + ' got : ' + JSON.stringify(element)));
+    });
+
+    done();
+  });
+
+  it('Should return 1 Error regarding apiSyntaxHeadlines errors', function(done) {
+    const str = '<h2 id="Syntax">Syntax</h2>' +
+      '<h3>Return value</h3>' +
+      '<h3>Errors thrown</h3>';
+
+    const expected = [
+      {msg: "invalid_headline_name", msgParams: ["Errors thrown"], type: ERROR}
+    ];
+
+    const expectedAfterFixing = [];
+
     let rootElement = document.createElement("body");
     rootElement.innerHTML = str;
 
