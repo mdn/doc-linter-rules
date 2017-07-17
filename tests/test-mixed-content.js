78da5f8b8ae1bac5c9ddf86e8227a7f5f25542f9
Anthony Maton <maitre.negi@gmail.com>
2017-07-17 12:22:44 +0200

    Replace href by src as per HTML Standard

--- tests/test-mixed-content.js
+++ tests/test-mixed-content.js
@@ -4,13 +4,13 @@ const ERROR = require('../lib/doctests.js').ERROR;
 
 describe('mixedContent', function() {
   it('Should return 2 Errors regarding mixed content', function(done) {
-    const str = '<img href="http://foo.bar/img.png">' +
-                '<img href="https://foor.bar/img.png">' +
-                '<img href="http://foo.bar/img.png?=https://">';
+    const str = '<img src="http://foo.bar/img.png">' +
+                '<img src="https://foor.bar/img.png">' +
+                '<img src="http://foo.bar/img.png?=https://">';
 
     const expected = [
-      {msg: '<img href="http://foo.bar/img.png">', type: ERROR},
-      {msg: '<img href="http://foo.bar/img.png?=https://">', type: ERROR}
+      {msg: '<img src="http://foo.bar/img.png">', type: ERROR},
+      {msg: '<img src="http://foo.bar/img.png?=https://">', type: ERROR}
     ];
 
     let rootElement = document.createElement("body");
