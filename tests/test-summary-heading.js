945b1b6d07645d3f85dab771138b58a3c7e50820
Anthony Maton <maitre.negi@gmail.com>
2017-07-25 13:10:02 +0200

    f

--- tests/test-summary-heading.js
+++ tests/test-summary-heading.js
@@ -5,10 +5,10 @@ const summaryHeading = require('../lib/summary-heading.js').summaryHeading;
 describe('summaryHeading', function() {
   it('Should return 1 errors regarding Summary heading', function(done) {
     const str = '<h2>Summary</h2>' +
-      '<h2 id="Summary" name="Summary">Summary</h2>' +
+      '<h2 id="Summary" name="Summary">Summary</h2>';
 
     const expected = [
-      {msg: '<h2>Summary</h2>', type: ERROR},
+      {msg: '<h2>Summary</h2>', type: ERROR}
     ];
 
     const expectedAfterFixing = [];
