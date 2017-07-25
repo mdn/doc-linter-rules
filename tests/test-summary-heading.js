2048c67d7d5551cb0235449f66317697a6415386
Anthony Maton <maitre.negi@gmail.com>
2017-07-25 12:53:35 +0200

    Improve summary heading testing

--- tests/test-summary-heading.js
+++ tests/test-summary-heading.js
@@ -4,17 +4,11 @@ const summaryHeading = require('../lib/summary-heading.js').summaryHeading;
 
 describe('summaryHeading', function() {
   it('Should return 4 errors regarding Summary heading', function(done) {
-    const str = 
-'<h2>Summary</h2>' +
+    const str = '<h2>Summary</h2>' +
       '<h2 id="Summary" name="Summary">Summary</h2>' +
-      '<h2 id="Summary" name="foo">Summary</h2>' +
-      '<h3 id="Summary">Summary</h3>';
 
     const expected = [
       {msg: '<h2>Summary</h2>', type: ERROR},
-      {msg: '<h2 id="Summary" name="Summary">Summary</h2>', type: ERROR},
-      {msg: '<h2 id="Summary" name="foo">Summary</h2>', type: ERROR},
-      {msg: '<h3 id="Summary">Summary</h3>', type: ERROR}
     ];
 
     const expectedAfterFixing = [];
