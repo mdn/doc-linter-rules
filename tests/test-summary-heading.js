d59d3835c9b99b02607c963a6ac4d3040e94745b
Anthony Maton <maitre.negi@gmail.com>
2017-08-09 12:25:20 +0200

    Improve the tests

--- tests/test-summary-heading.js
+++ tests/test-summary-heading.js
@@ -3,12 +3,27 @@ const ERROR = require('../lib/doctests.js').ERROR;
 const summaryHeading = require('../lib/summary-heading.js').summaryHeading;
 
 describe('summaryHeading', function() {
-  it('Should return 1 errors regarding Summary heading', function(done) {
-    const str = '<h2>Summary</h2>' +
-      '<h2 id="Summary" name="Summary">Summary</h2>';
+  it('Should return 6 errors regarding Summary heading', function(done) {
+    const str = '<h1>Summary</h1>' +
+      '<h1 id="Summary" name="Summary">Summary</h1>'
+      '<h2>Summary</h2>' +
+      '<h2 id="Summary" name="Summary">Summary</h2>'
+      '<h3>Summary</h3>' +
+      '<h3 id="Summary" name="Summary">Summary</h3>' + 
+      '<h4>Summary</h4>' +
+      '<h4 id="Summary" name="Summary">Summary</h4>' + 
+      '<h5>Summary</h5>' +
+      '<h5 id="Summary" name="Summary">Summary</h5>' +
+      '<h6>Summary</h6>' +
+      '<h6 id="Summary" name="Summary">Summary</h6>';
 
     const expected = [
-      {msg: '<h2>Summary</h2>', type: ERROR}
+      {msg: '<h1>Summary</h1>', type: ERROR},
+      {msg: '<h2>Summary</h2>', type: ERROR},
+      {msg: '<h3>Summary</h3>', type: ERROR},
+      {msg: '<h4>Summary</h4>', type: ERROR},
+      {msg: '<h5>Summary</h5>', type: ERROR},
+      {msg: '<h6>Summary</h6>', type: ERROR}
     ];
 
     const expectedAfterFixing = [];
